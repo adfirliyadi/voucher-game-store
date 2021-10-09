7d985c7ba3ca3416bcf9b3ec200974a7b3f7adf0
adfirliyadi <firliyadi@gmail.com>
2021-10-10 00:47:18 +0700

    Convert member page html to nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,12 @@ import '../styles/sign-in.css';
 import '../styles/sign-up.css';
 import '../styles/sign-up-photo.css';
 import '../styles/sign-up-success.css';
+import '../styles/404-not-found.css';
+import '../styles/sidebar.css';
+import '../styles/overview.css';
+import '../styles/transactions.css';
+import '../styles/transactions-detail.css';
+import '../styles/edit-profile.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
