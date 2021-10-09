31adf5ee361d2d39a5043b749d3b237ec91b7b4d
adfirliyadi <firliyadi@gmail.com>
2021-10-09 23:51:00 +0700

    Convert sign up html to nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,9 @@ import '../styles/detail.css';
 import '../styles/checkout.css';
 import '../styles/complete-checkout.css';
 import '../styles/sign-in.css';
+import '../styles/sign-up.css';
+import '../styles/sign-up-photo.css';
+import '../styles/sign-up-success.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
