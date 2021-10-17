93243825334b95e784833dad5bf13074926a6697
adfirliyadi <firliyadi@gmail.com>
2021-10-17 11:06:03 +0700

    Refactor checkout page

---
+++ components/organisme/CheckoutConfirmation/index.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function CheckoutConfirmation() {
+    return (
+        <>
+            <label className="checkbox-label text-lg color-palette-1">I have transferred the money
+                <input type="checkbox" />
+                <span className="checkmark"></span>
+            </label>
+            <div className="d-md-block d-flex flex-column w-100 pt-50">
+                <Link href="/complete-checkout" >
+                    <a className="btn btn-confirm-payment rounded-pill fw-medium text-white border-0 text-lg"
+                        role="button">Confirm
+                        Payment
+                    </a>
+                </Link>
+            </div>
+        </>
+    )
+}
